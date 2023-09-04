18bfa226a0e52c7cce6757bf5d8852937fb19e10
OnePunchManatee <94397888+OnePunchManatee@users.noreply.github.com>
2023-09-03 20:43:14 -0500

    Add files via upload

--- cssanimation/cssanimation.js
+++ cssanimation/cssanimation.js
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function () {
     confirm = document.getElementById("autoplaybaniscringe");
     confirm.addEventListener("click", function(){
         hypnodrone.play();
-        confirm.style.zIndex = "-99";
         
     });
     manatee.addEventListener("click", function(){
